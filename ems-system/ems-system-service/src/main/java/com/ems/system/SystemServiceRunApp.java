6b9e1756015c6d2d4b558689a2a92bc4c07a5410
holysword02 <l982370753@outlook.com>
2023-09-26 14:27:08 +0800

    0.1.2.6
    分页查询

--- ems-system/ems-system-service/src/main/java/com/ems/system/SystemServiceRunApp.java
+++ ems-system/ems-system-service/src/main/java/com/ems/system/SystemServiceRunApp.java
@@ -1,12 +1,14 @@
 package com.ems.system;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScans(@ComponentScan("com.ems.config"))
 public class SystemServiceRunApp {
     public static void main(String[] args) {
         SpringApplication.run(SystemServiceRunApp.class, args);
--- ems-system/ems-system-service/src/main/java/com/ems/system/controller/UserController.java
+++ ems-system/ems-system-service/src/main/java/com/ems/system/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.ems.system.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ems.api.domain.po.User;
-import com.ems.system.service.IUserService;
-import lombok.RequiredArgsConstructor;
+import com.ems.system.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +14,11 @@ import java.util.Date;
 @RestController
 @CrossOrigin
 @RequestMapping("/user")
-@RequiredArgsConstructor
 public class UserController {
     @Autowired
-    private final IUserService IUserService;
+    private UserServiceImpl userService;
 
-    //新增用户
+    // 新增用户
     @PostMapping("/insert")
     public boolean insert(@RequestBody User user) {
         String password = DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
@@ -26,18 +26,25 @@ public class UserController {
         Date date = new Date();
         user.setCreateTime(date);
         user.setUpdateTime(date);
-        return IUserService.save(user);
+        return userService.save(user);
     }
 
-    //删除用户   状态变为停用
+    // 删除用户   状态变为停用
     @DeleteMapping("/delete/{id}")
     public boolean del(@PathVariable Serializable id, @RequestHeader("Cookie") String cookie) {
-        return IUserService.del(id, cookie);
+        return userService.del(id, cookie);
     }
 
-    //修改密码
+    // 修改密码
     @PutMapping("/update/passwd")
     public boolean updatePassWd(@RequestBody User user, @RequestHeader("Cookie") String cookie) {
-        return IUserService.updatePassWd(user, cookie);
+        return userService.updatePassWd(user, cookie);
+    }
+
+    // 分页查询
+    @GetMapping("/find")
+    public IPage<User> find(Integer pageNum, Integer pageSize) {
+        IPage<User> ip = new Page<>(pageNum, pageSize);
+        return userService.page(ip);
     }
 }
