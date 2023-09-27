cb9ca6028485eef137554e0288884437fa23f3d6
holysword02 <l982370753@outlook.com>
2023-09-27 14:32:56 +0800

    Update DictController.java

--- ems-basic/ems-basic-service/src/main/java/com/ems/basic/controller/DictController.java
+++ ems-basic/ems-basic-service/src/main/java/com/ems/basic/controller/DictController.java
@@ -1,10 +1,12 @@
 package com.ems.basic.controller;
 
+import com.ems.basic.entity.Dict;
 import com.ems.basic.service.IDictService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.Serializable;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -15,19 +17,26 @@ public class DictController {
     private final IDictService dictService;
 
     //    新增
-    public boolean insert() {
-
+    @PostMapping("/save")
+    public boolean insert(Dict dict) {
+        return dictService.save(dict);
     }
 
     //    删除
-    public boolean del() {
-
+    @DeleteMapping("/delete/{id}")
+    public boolean del(@PathVariable Serializable id) {
+        return dictService.removeById(id);
     }
 
     //    修改
-    public boolean update() {
-
+    @PutMapping("/update")
+    public boolean update(Dict dict) {
+        return dictService.updateById(dict);
     }
-//    查询
-    public
+
+    //    查询
+    @GetMapping("/list")
+    public List<Dict> getAll() {
+        return dictService.list();
+    }
 }
