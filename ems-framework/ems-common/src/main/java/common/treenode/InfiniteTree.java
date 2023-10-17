ce8f8f3eb86bcd1a93c7dbfc394cf71cef3044ea
holysword02 <l982370753@outlook.com>
2023-10-17 23:32:17 +0800

    Update InfiniteTree.java

--- ems-framework/ems-common/src/main/java/common/treenode/InfiniteTree.java
+++ ems-framework/ems-common/src/main/java/common/treenode/InfiniteTree.java
@@ -46,10 +46,10 @@ public class InfiniteTree<M extends BaseMapper<X>, X> {
 
     private List<TreeNode> createTree(List<TreeNode> treeNodeList, List<X> list, int n,
                                       String column, String methodName,
-                                      String treeValueName, String treeLabelName,
-                                      String nodeValueName, String nodeLabelName) {
+                                      String valueName, String labelName) {
+        if (n == 0) return treeNodeList;
         if (!list.isEmpty()) {
-            n++;
+            n--;
             for (X x : list) {
                 QueryWrapper<X> qwNode = getQw();
                 List<TreeNode> node = new ArrayList<>();
@@ -59,20 +59,12 @@ public class InfiniteTree<M extends BaseMapper<X>, X> {
                     throw new RuntimeException(e);
                 }
                 List<X> nodeList = mapper.selectList(qwNode);
-                createTree(node, nodeList, n, column, methodName, treeValueName, treeLabelName, nodeValueName, nodeLabelName);
+                createTree(node, nodeList, n, column, methodName, valueName, labelName);
                 try {
-                    //TODO 判断是否为根节点
-                    if (n % 2 == 1) {
-                        treeNodeList.add(new TreeNode(
-                                x.getClass().getMethod("get" + toU(treeValueName)).invoke(x).toString(),
-                                x.getClass().getMethod("get" + toU(treeLabelName)).invoke(x).toString(),
-                                node));
-                    } else {
-                        treeNodeList.add(new TreeNode(
-                                x.getClass().getMethod("get" + toU(nodeValueName)).invoke(x).toString(),
-                                x.getClass().getMethod("get" + toU(nodeLabelName)).invoke(x).toString(),
-                                node));
-                    }
+                    treeNodeList.add(new TreeNode(
+                            x.getClass().getMethod("get" + toU(valueName)).invoke(x).toString(),
+                            x.getClass().getMethod("get" + toU(labelName)).invoke(x).toString(),
+                            node));
                 } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
                     throw new RuntimeException(e);
                 }
@@ -92,26 +84,23 @@ public class InfiniteTree<M extends BaseMapper<X>, X> {
                                           String column, String methodName,
                                           String valueName, String labelName) {
         List<TreeNode> treeNodeList = new ArrayList<>();
-        return createTree(treeNodeList, list, 0, column, methodName, valueName, labelName, valueName, labelName);
+        return createTree(treeNodeList, list, 0, column, methodName, valueName, labelName);
     }
 
     /**
-     * 二级树形
+     * n级树形
      *
      * @param list          根节点列表
      * @param column        子节点判断条件的列的名称
      * @param methodName    子节点判断条件的上级节点的属性名称
      * @param treeValueName 树形根节点value对应的属性名
      * @param treeLabelName 树形根节点label对应的属性名
-     * @param nodeValueName 树形子节点value对应的属性名
-     * @param nodeLabelName 树形子节点label对应的属性名
      */
-    public List<TreeNode> getTreeNodeList(List<X> list,
+    public List<TreeNode> getTreeNodeList(List<X> list, int n,
                                           String column, String methodName,
-                                          String treeValueName, String treeLabelName,
-                                          String nodeValueName, String nodeLabelName) {
+                                          String treeValueName, String treeLabelName) {
         List<TreeNode> treeNodeList = new ArrayList<>();
-        return createTree(treeNodeList, list, 0, column, methodName, treeValueName, treeLabelName, nodeValueName, nodeLabelName);
+        return createTree(treeNodeList, list, n, column, methodName, treeValueName, treeLabelName);
     }
 
 }
